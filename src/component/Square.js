2392d8c5cab9d14349e82464ac9a4973291cb61a
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2023-08-06 00:03:01 +0530

    This is tictactoe game using react js

---
+++ src/component/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Square = (props) => {
+  return (
+    <>
+      <div
+        onClick={props.onClick}
+        style={{ border: '1px solid', height: '100px', width: '100%' }}
+      >
+        <h5>{props.value}</h5>
+      </div>
+    </>
+  );
+};
+
+export default Square;
